97c35d7d2774c9cc13824efd1918d4e9b7d0da24
ognn <ogunozalp1991@gmail.com>
2023-01-17 23:36:07 +0300

    docker-compose

--- src/main/scala/org/binance/StructuredStreaming.scala
+++ src/main/scala/org/binance/StructuredStreaming.scala
@@ -1,10 +1,9 @@
 package org.binance
 
-import org.apache.spark.sql.functions.{col, from_json, from_unixtime, window}
+import org.apache.spark.sql.functions.{arrays_zip, col, from_json}
 import org.apache.spark.sql.streaming.Trigger
-import org.apache.spark.sql.types.TimestampType
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.binance.data.Schema.tradeStreamsSchema
+import org.binance.data.Schema.arrayArraySchema
 import org.binance.spark.VWAPCombiner
 
 import java.util.concurrent.TimeUnit
@@ -54,7 +53,7 @@ object StructuredStreaming {
       .builder
       .appName("BinanceStreaming")
       .config("spark.sql.caseSensitive" , "True")
-      .config("spark.sql.streaming.checkpointLocation","/tmp/blockchain-streaming/sql-streaming-checkpoint")
+      //.config("spark.sql.streaming.checkpointLocation","/tmp/blockchain-streaming/sql-streaming-checkpoint")
       .master("local[4]")
       .getOrCreate()
 
@@ -64,19 +63,25 @@ object StructuredStreaming {
     spark.sparkContext.setLogLevel("ERROR")
 
     val tradeStream = spark
-      .readStream
+      .read
       .format("kafka")
       .option("subscribe", "binance")
       .option("kafka.bootstrap.servers", "localhost:9092")
+      .option("startingOffsets", "earliest")
       .load
       .selectExpr("cast(value as string) as value") //casting binary values into string
-      .select(from_json(col("value"), tradeStreamsSchema).alias("tmp")).select("tmp.*")
-      .withColumn("p",  col("p").cast("double"))
-      .withColumn("q",  col("q").cast("double"))
-      .withColumn("pq", col("p") * col("q"))
-      .withColumn("T", from_unixtime(col("T").cast ("bigint")/1000).cast(TimestampType))
-//      .withColumn("T", unix_timestampmp($"T", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").cast(TimestampType))
-      .withWatermark("T", "5 seconds")
+      .select(from_json(col("value"), arrayArraySchema).alias("tmp")).select(col("tmp.lastUpdateId"), arrays_zip(col("tmp.bids"),col("tmp.asks")))
+
+
+
+    tradeStream.show(false)
+//    tradeStream.printSchema()
+//      .withColumn("p",  col("p").cast("double"))
+//      .withColumn("q",  col("q").cast("double"))
+//      .withColumn("pq", col("p") * col("q"))
+//      .withColumn("T", from_unixtime(col("T").cast ("bigint")/1000).cast(TimestampType))
+////      .withColumn("T", unix_timestampmp($"T", "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").cast(TimestampType))
+//      .withWatermark("T", "5 seconds")
     //          .as[TradeStreams] //Enable to do any Dataset operations
 
     tradeStream.printSchema()
@@ -84,49 +89,49 @@ object StructuredStreaming {
     tradeStream.createOrReplaceTempView("trade_stream")
 
 
-    val sql = "SELECT s as key, SUM(pq) as sum_pq, SUM(q) as sum_q FROM trade_stream GROUP BY s"
-
-    val vwapOverSqlStatement = spark
-      .sql(sql)
-      .withColumn("vwapOverSqlStatement",  col("sum_pq") /  col("sum_q"))
-      .withColumn("value",  col("vwapOverSqlStatement").cast("string"))
-
-    vwapOverSqlStatement.printSchema()
-
-    val vwap = tradeStream
-      .groupBy(
-        window(col("T"), "10 seconds", "5 seconds"),
-        col("s")
-      ).sum("pq", "q")
+//    val sql = "SELECT s as key, SUM(pq) as sum_pq, SUM(q) as sum_q FROM trade_stream GROUP BY s"
+//
+//    val vwapOverSqlStatement = spark
+//      .sql(sql)
+//      .withColumn("vwapOverSqlStatement",  col("sum_pq") /  col("sum_q"))
+//      .withColumn("value",  col("vwapOverSqlStatement").cast("string"))
+//
+//    vwapOverSqlStatement.printSchema()
+//
+//    val vwap = tradeStream
+//      .groupBy(
+//        window(col("T"), "10 seconds", "5 seconds"),
+//        col("s")
+//      ).sum("pq", "q")
 
     //(run-main-1) org.apache.spark.sql.AnalysisException: Multiple streaming aggregations are not supported with streaming DataFrames/Datasets;;
     //.groupBy("s")
     //.sum("sum(pq)", "sum(q)")
 
-
-    vwapOverSqlStatement
-      .writeStream
-      .format("kafka")
-      .outputMode("complete")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("topic","vwap")
-      .option("checkpointLocation", "/tmp/blockchain-streaming/sql-streaming-checkpoint/vwap/")
-      .start()
-
-    spark
-      .readStream
-      .format("kafka")
-      .option("subscribe","vwap")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .load
-      .selectExpr("cast(value as string) as vwapFromSparkStreaming") //casting binary values into string
-      .writeStream
+//
+//    tradeStream
+//      .writeStream
+//      .format("kafka")
 //      .outputMode("complete")
-      .format("console")
-      //.trigger(Trigger.Continuous(batchTimeInSeconds, TimeUnit.SECONDS))
-      .option("truncate",false)
-      .start()
-      .awaitTermination()
+//      .option("kafka.bootstrap.servers", "localhost:9092")
+//      .option("topic","vwap")
+//      .option("checkpointLocation", "/tmp/blockchain-streaming/sql-streaming-checkpoint/vwap/")
+//      .start()
+//
+//    spark
+//      .readStream
+//      .format("kafka")
+//      .option("subscribe","vwap")
+//      .option("kafka.bootstrap.servers", "localhost:9092")
+//      .load
+//      .selectExpr("cast(value as string) as vwapFromSparkStreaming") //casting binary values into string
+//      .writeStream
+////      .outputMode("complete")
+//      .format("console")
+//      //.trigger(Trigger.Continuous(batchTimeInSeconds, TimeUnit.SECONDS))
+//      .option("truncate",false)
+//      .start()
+//      .awaitTermination()
   }
 }
 
--- src/main/scala/org/binance/data/Schema.scala
+++ src/main/scala/org/binance/data/Schema.scala
@@ -1,73 +1,13 @@
 package org.binance.data
 
-import org.apache.spark.sql.types.{StringType, StructType, TimestampType}
+import org.apache.spark.sql.types.{ArrayType, StringType, StructType}
 import spray.json.DefaultJsonProtocol._
 import spray.json._
 
 object Schema {
 
-
-
-  val tradeStreamsSchema = new StructType()
-    .add("e", StringType)
-    .add("E", StringType)
-    .add("s", StringType)
-    .add("t", StringType)
-    .add("p", StringType)
-    .add("q", StringType)
-    .add("b", StringType)
-    .add("a", StringType)
-    .add("T", TimestampType)
-    .add("m", StringType)
-    .add("M", StringType)
-
-
-  case class TradeStreams(e: String,
-                          E: String,
-                          s: String,
-                          t: String,
-                          p: Double,
-                          q: Double,
-                          b: String,
-                          a: String,
-                          T: String,
-                          m: String,
-                          M: String)
-
-
-
-  def parseTradeStreams(line: String):TradeStreams = {
-
-    val res = line
-      .replace("{", "")
-      .replace("}", "")
-      .replace("\"", "")
-      .split(",")
-      .map(field => {
-        val res = field.split(":")
-        (res(0).toString, res(1).toString)
-      })
-      .toMap
-
-    TradeStreams(e = res.getOrElse("e", "0"),
-      E = res.getOrElse("E", "0"),
-      s = res.getOrElse("s", "0"),
-      t = res.getOrElse("t", "0"),
-      p = res.getOrElse("p", "0").toDouble,
-      q = res.getOrElse("q", "0").toDouble,
-      b = res.getOrElse("b","0"),
-      a = res.getOrElse("a", "0"),
-      T = res.getOrElse("T", "0"),
-      m = res.getOrElse("m", "0"),
-      M = res.getOrElse("M", "0"))
-  }
-
-  //TODO check why it fails to parse E, for which the value is Long not String
-  object TradeStreamsProtocol extends DefaultJsonProtocol {
-    implicit val tradeStreamsFormat = jsonFormat11(TradeStreams)
-  }
-
-
-
+  val arrayArraySchema = new StructType().add("lastUpdateId", StringType)
+    .add("bids", ArrayType(ArrayType(StringType)))
+    .add("asks", ArrayType(ArrayType(StringType)))
 
 }
